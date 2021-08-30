c41ac1f110c83d5a2821cd423fa0bca40673517b
Victor Andrade <65470967+vaps41@users.noreply.github.com>
2021-08-30 02:45:29 -0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+function abrirModal() {
+    document.getElementById('modal').style.top = "auto";
+    document.getElementById('modal').style.display = "block";
+}
+function fecharModal() {
+    document.getElementById('modal').style.top = "-100%";
+}
